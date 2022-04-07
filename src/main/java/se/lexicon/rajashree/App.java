0c268f501d7396a6f81625d05f06ac33cb45d950
Rajashree <bhuyan.rajashree@gmail.com>
2022-04-07 23:20:27 +0200

    Oops Exercise

--- src/main/java/se/lexicon/rajashree/App.java
+++ src/main/java/se/lexicon/rajashree/App.java
@@ -4,5 +4,12 @@ public class App {
     public static void main(String[] args){
         Car car = new Car("BMW","X70");
         car.drive(car);
+
+        Rectangle rectangle = new Rectangle(20.8,10.7);
+
+        double area = rectangle.getArea(rectangle.getHeight(), rectangle.getWidth());
+
+        System.out.println(area);
+
     }
 }
